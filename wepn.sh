586cd1b5188ee1e2259130ee771eb4defaed0095
macromicro <39789381+elemen3@users.noreply.github.com>
2023-08-11 09:06:24 +0330

    fix: the bug which was causing the terminal to become fuzzy

--- wepn.sh
+++ wepn.sh
@@ -491,6 +491,7 @@ update_upgrade_package_lists(){
 
   print center "[blue]Upgrading packages..."
   apt_upgrade_error=$(apt-get upgrade -y 2>&1 >/dev/null)
+  clear_logs 1
 
 apt-get upgrade -y -q 2>&1 >/dev/null
 
